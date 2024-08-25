3445f27141d74c09ad6956d5afa4b89502cb643d
VIMO2024 <167612772+VIMO2024@users.noreply.github.com>
2024-08-25 21:51:33 +0200

    Delete picture/picture directory

--- picture/picture/src/main/java/de/vimo/Picture.java
+++
@@ -1,8 +0,0 @@
-package de.vimo;
-
-public interface Picture {
-    String getPictureMotive();
-    double getPictureLength();
-    double getPictureHeight();
-    void switchPicturePosition(int a, int b);
-}
